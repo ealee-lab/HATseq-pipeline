18c80ea3587bc24c7365224e9d586d8687a783e0
Shayna Mallett <slmallet@asu.edu>
2024-08-20 14:19:03 -0400

    Adding updates after running a sample through to test it works

---
+++ setup/conda_create.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+conda create --file conda.yml -p ./HATseq
--- workflow/run_HATseq.sh
+++ workflow/run_HATseq.sh
@@ -10,51 +10,15 @@
 #SBATCH --error=./Hatseq_%j.err
 #SBATCH --output=./Hatseq_%j.out
 
-
+#Activate environment 
 source activate HATseq_star 
 export PATH=~/tools/seqtk:~/tools:~/tools/fastp:$PATH
 
-snakefile=/lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/workflow/Snakefile.hg38 #_test_rescue #_change_order #_no_blast #main #.test #main
-profile=/lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/workflow/slurm
-configfile=/lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/config/config.*.$1  #/n/scratch3/users/s/sm745/HATseq_all/config/config.single_cell.$1
+snakefile=/lab-share/Gene-Lee-e2/Public/home/shayna/HATseq-pipeline/workflow/Snakefile.hg38 #_test_rescue #_change_order #_no_blast #main #.test #main
+profile=/lab-share/Gene-Lee-e2/Public/home/shayna/HATseq-pipeline/workflow/slurm
+configfile=/lab-share/Gene-Lee-e2/Public/home/shayna/HATseq-pipeline/config/config.*.$1  #/n/scratch3/users/s/sm745/HATseq_all/config/config.single_cell.$1
 directory=/lab-share/Gene-Lee-ANR-e2/shayna/data/output/$1 #/lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/workflow/$1
 
-
-
 snakemake --unlock -s ${snakefile}  --profile ${profile}  --configfile ${configfile}  --directory ${directory}
-snakemake   -s ${snakefile}  --profile ${profile}  --configfile ${configfile}  --directory ${directory} 
+snakemake -s ${snakefile}  --profile ${profile}  --configfile ${configfile}  --directory ${directory} 
 
-
-#snakemake  -c 1    -s ${snakefile}   --configfile ${configfile}  --directory ${directory}
-
-#snakemake -s /home/sm745/HATseq_all/HATseq_v2/workflow/Snakefile.main --profile /home/sm745/HATseq_all/HATseq_v2/workflow/slurm --configfile /n/scratch3/users/s/sm745/HATseq_all/config/config.single_cell.$1 --directory /n/scratch3/users/s/sm745/HATseq_all/analysis_v2_1300/$1
-
-
-
-#snakemake --unlock -s /home/sm745/HATseq_all/HATseq_v2/workflow/Snakefile.module3 --profile /home/sm745/HATseq_all/HATseq_v2/workflow/slurm --configfile /n/scratch3/users/s/sm745/HATseq_all/config/config.single_cell.$1 --directory /n/scratch3/users/s/sm745/HATseq_all/analysis_v2_1300/$1
-
-#snakemake -s /home/sm745/HATseq_all/HATseq_v2/workflow/Snakefile.module3 --profile /home/sm745/HATseq_all/HATseq_v2/workflow/slurm --configfile /n/scratch3/users/s/sm745/HATseq_all/config/config.single_cell.$1 --directory /n/scratch3/users/s/sm745/HATseq_all/analysis_v2_1300/$1
-
-
-
-#snakemake --unlock -s /home/sm745/HATseq_all/HATseq_v2/workflow/Snakefile.module4 --profile /home/sm745/HATseq_all/HATseq_v2/workflow/slurm --configfile /n/scratch3/users/s/sm745/HATseq_all/config/config.single_cell.$1 --directory /n/scratch3/users/s/sm745/HATseq_all/analysis_v2_1300/$1
-
-#snakemake -s /home/sm745/HATseq_all/HATseq_v2/workflow/Snakefile.module4 --profile /home/sm745/HATseq_all/HATseq_v2/workflow/slurm --configfile /n/scratch3/users/s/sm745/HATseq_all/config/config.single_cell.$1 --directory /n/scratch3/users/s/sm745/HATseq_all/analysis_v2_1300/$1
-
-#snakemake --unlock -s /home/sm745/HATseq_all/HATseq_v2/workflow/Snakefile.module5 --profile /home/sm745/HATseq_all/HATseq_v2/workflow/slurm --configfile /n/scratch3/users/s/sm745/HATseq_all/config/config.single_cell.$1 --directory /n/scratch3/users/s/sm745/HATseq_all/analysis_v2_1300/$1
-
-#snakemake -s /home/sm745/HATseq_all/HATseq_v2/workflow/Snakefile.module5 --profile /home/sm745/HATseq_all/HATseq_v2/workflow/slurm --configfile /n/scratch3/users/s/sm745/HATseq_all/config/config.single_cell.$1 --directory /n/scratch3/users/s/sm745/HATseq_all/analysis_v2_1300/$1
-
-#snakemake --unlock -s /home/sm745/HATseq_all/HATseq_v2/workflow/Snakefile.module6 --profile /home/sm745/HATseq_all/HATseq_v2/workflow/slurm --configfile /n/scratch3/users/s/sm745/HATseq_all/config/config.single_cell.$1 --directory /n/scratch3/users/s/sm745/HATseq_all/analysis_v2_1300/$1
-
-#snakemake -s /home/sm745/HATseq_all/HATseq_v2/workflow/Snakefile.module6 --profile /home/sm745/HATseq_all/HATseq_v2/workflow/slurm --configfile /n/scratch3/users/s/sm745/HATseq_all/config/config.single_cell.$1 --directory /n/scratch3/users/s/sm745/HATseq_all/analysis_v2_1300/$1
-
-
-
-
-
-#snakemake -s /lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/workflow/Snakefile.module6 --profile /lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/workflow/slurm --configfile /lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/config/config.single_cell --directory /lab-share/Gene-Lee-e2/Public/home/shayna/5666_BA9_1
-
-#snakemake -s /lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/workflow/Snakefile.module3 --profile /lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/workflow/slurm --configfile /lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/config/config --directory /lab-share/Gene-Lee-e2/Public/home/shayna/5666_BA9_1 
-
-#snakemake -s /lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/workflow/Snakefile.module2 --profile /lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/workflow/slurm --configfile /lab-share/Gene-Lee-e2/Public/home/shayna/HATseq_v2/config/config --directory /lab-share/Gene-Lee-e2/Public/home/shayna/5666_BA9_1
