4f5c830fa29f40c03e5361a367273a48056b17f4
rogadde <rohinigadde@gmail.com>
2025-07-22 11:17:50 -0400

    Correct precision calculations for noerror

--- workflow/calculate_stats.sh
+++ workflow/calculate_stats.sh
@@ -6,14 +6,19 @@ pdir=$1 # Parent directory containing all pipeline output
 truth_name=$2 # Name of ground truth set
 truth_peaks=$3 # Ground truth set (GTS) of peaks
 
-for call_peaks in $(ls ${pdir}/*/analysis/*big_table_filter_reasons.tsv); do
-    sample=$(basename "${call_peaks}" _big_table_filter_reasons.tsv)
-    outdir=$(dirname "${call_peaks}")
+num_truth=$(wc -l "${truth_peaks}" | awk '{print $1}')
+
+for path in $(ls ${pdir}/); do
+    sample=$(basename "${path}")
+    outdir=$(realpath "${pdir}")/${sample}/analysis/post
     echo $sample
 
+    # BEFORE ERROR-PRONE FILTER
+    call_peaks="${pdir}/${sample}/analysis/${sample}_filtered_peaks.tsv"
+    both_peaks="${outdir}/${sample}_big_table_filter_${truth_name}.tsv"
+
+    # Number of called peaks
     num_called=$(grep -E 'UNK|SOM' "${call_peaks}" | wc -l)
-    num_truth=$(wc -l "${truth_peaks}" | awk '{print $1}')
-    both_peaks="${outdir}/${sample}_big_table_filter_${truth_name}_noerror.tsv"
 
     # Number of true peaks found - don't recount if 1 called peak overlaps multiple true peaks
     tp=$(grep -E 'UNK|SOM' "${both_peaks}" | awk '{print $4}' | sort | uniq | wc -l) 
@@ -25,12 +30,34 @@ for call_peaks in $(ls ${pdir}/*/analysis/*big_table_filter_reasons.tsv); do
     recall=$(echo "scale=4; ${tp} / (${tp} + ${fn})" | bc) 
     f1=$(echo "scale=4; 2 * (${precision} * ${recall}) / (${precision} + ${recall})" | bc)
 
-    outfile="${outdir}/${sample}_f1_score_${truth_name}_noerror.txt"
-    echo -e "${sample}\n" > "${outfile}"
+    outfile="${outdir}/${sample}_f1_score_${truth_name}.txt"
+    echo -e "Number of true insertions: ${num_truth}\n" > "${outfile}"
+    echo -e "BEFORE ERROR-PRONE FILTER: ${sample}" >> "${outfile}"
     echo "Number of UNK/SOM peaks called by HAT-seq: ${num_called}" >> "${outfile}"
-    echo "Number of peaks in ground truth set: ${num_truth}" >> "${outfile}"
     echo "Number of true peaks found by HAT-seq: ${tp}" >> "${outfile}"
     echo "Precision: ${precision}" >> "${outfile}"
     echo "Recall: ${recall}" >> "${outfile}"
     echo "F1-score: ${f1}" >> "${outfile}"
+
+
+    # AFTER ERROR-PRONE FILTER
+    call_peaks_noerr="${outdir}/${sample}_big_table_filter_noerror.tsv"
+    both_peaks_noerr="${outdir}/${sample}_big_table_filter_${truth_name}_noerror.tsv"
+
+    num_called_noerr=$(grep -E 'UNK|SOM' "${call_peaks_noerr}" | wc -l)
+    tp_noerr=$(grep -E 'UNK|SOM' "${both_peaks_noerr}" | awk '{print $4}' | sort | uniq | wc -l) 
+
+    fp_noerr=$((${num_called_noerr} - ${tp_noerr}))
+    fn_noerr=$((${num_truth} - ${tp_noerr}))
+
+    precision_noerr=$(echo "scale=4; ${tp_noerr} / (${tp_noerr} + ${fp_noerr})" | bc) 
+    recall_noerr=$(echo "scale=4; ${tp_noerr} / (${tp_noerr} + ${fn_noerr})" | bc) 
+    f1_noerr=$(echo "scale=4; 2 * (${precision_noerr} * ${recall_noerr}) / (${precision_noerr} + ${recall_noerr})" | bc)
+
+    echo -e "\nAFTER ERROR-PRONE FILTER: ${sample}" >> "${outfile}"
+    echo "Number of UNK/SOM peaks called by HAT-seq: ${num_called_noerr}" >> "${outfile}"
+    echo "Number of true peaks found by HAT-seq: ${tp_noerr}" >> "${outfile}"
+    echo "Precision: ${precision_noerr}" >> "${outfile}"
+    echo "Recall: ${recall_noerr}" >> "${outfile}"
+    echo "F1-score: ${f1_noerr}" >> "${outfile}"
 done
--- workflow/compare_truth_and_filter_error.sh
+++ workflow/compare_truth_and_filter_error.sh
@@ -13,18 +13,25 @@ for call_set in $(ls ${pdir}/*/analysis/*_big_table_filter_reasons.tsv); do
     outdir=$(dirname "${call_set}")
     echo $sample
 
-    truth_out="${outdir}/${sample}_big_table_filter_${truth_name}.tsv"
-    error_out="${outdir}/${sample}_big_table_filter_${truth_name}_noerror.tsv"
+    mkdir -p "${outdir}/post"
+    noerr_out="${outdir}/post/${sample}_big_table_filter_noerror.tsv"
+    truth_out="${outdir}/post/${sample}_big_table_filter_${truth_name}.tsv"
+    truth_noerr_out="${outdir}/post/${sample}_big_table_filter_${truth_name}_noerror.tsv"
 
-    # Extract relevant fields from peak set then intersect with truth set
+    # Extract relevant fields from peak set then intersect
     # Fields extracted are chrm, start, end, peak, RPM, strand, classification, filter_reason
+    awk -v FS='\t' -v OFS='\t' '{if (NR > 1) print $2,$3,$4,$5,$27,$7,$51,$52}' "${call_set}" | \
+        bedtools intersect -v -a stdin \
+        -b "${error_prone}" \
+	    > "${noerr_out}"
+
     awk -v FS='\t' -v OFS='\t' '{if (NR > 1) print $2,$3,$4,$5,$27,$7,$51,$52}' "${call_set}" | \
         bedtools intersect -wo -S -a stdin \
 	    -b <(bedtools slop -b 50 -i "${truth_set}" -g "${genome}") \
 	    > "${truth_out}"
 
-    # Remove peaks overlapping error-prone regions
+    # Remove peaks overlapping error-prone regions after intersecting GTS
     bedtools intersect -v -a "${truth_out}" \
         -b "${error_prone}" \
-	    > "${error_out}"
+	    > "${truth_noerr_out}"
 done
