be2b46fe1511e02b1684bd40e012c63870a4cf21
rogadde <rohinigadde@gmail.com>
2025-08-04 10:09:06 -0400

    Update post-processing and clean up unused scripts

--- workflow/calculate_stats.sh
+++ workflow/calculate_stats.sh
@@ -2,82 +2,31 @@
 
 # Calculate precision, recall, and F1 score for each sample.
 
-peak_file=$1 # Path to input peaks intersected with ground truth set (GTS)
-noerr_file=$2 # Path to input peaks intersected with GTS and error-prone filtered
+peak_file=$1 # Path to filtered_peaks.tsv intersected with ground truth set (GTS)
+true_peaks=$2 # Path to GTS
 out_file=$3 # Path to output file with statistics
-true_peaks=$4 # Path to GTS
-names=$5 # List of peaks to remove from total count
 
+# Number of false positives (i.e. number of peaks that overlap with GTS) 
+fp=$(grep -E 'UNK|SOM' "${peak_file}" | \
+    awk -v FS='\t' '{if ($19 == "-NA-") print $4}' | wc -l)
+
+# Number of true peaks found (i.e. number of peaks that overlap with GTS)
+# (Don't recount if 1 called peak overlaps >1 true peaks)
+tp=$(awk -v FS='\t' '{print $4}' "${true_peaks}" | grep -f - "${peak_file}" | \
+    grep -E 'UNK|SOM' | awk -v FS='\t' '{if ($19 != "-NA-") print $4}' | \
+    sort | uniq | wc -l) 
+
+num_called=$((${tp} + ${fp}))
 num_true=$(wc -l "${true_peaks}" | awk '{print $1}')
-
-#### BEFORE ERROR-PRONE FILTER ####
-
-# If names of insertions are given, remove them from the count
-# e.g. If using somatic GTS, remove germline GTS insertions from count to subset
-if [[ $# == 5 ]]; then
-
-    # Number of called peaks (don't recount if 1 called peak overlaps >1 true peaks)
-    num_called=$(grep -v -f "${names}" "${peak_file}" | grep -E 'UNK|SOM' | \
-        awk -v FS='\t' '{print $4}' | sort | uniq | wc -l)
-
-    # Number of true peaks found (i.e. number of peaks that overlap with GTS)
-    tp=$(grep -v -f "${names}" "${peak_file}" | grep -E 'UNK|SOM' | \
-        awk -v FS='\t' -v OFS='\t' '{if ($15 > 0) print $4,$7}' | \
-        awk -v FS='\t' '{print $1}' | sort | uniq | wc -l) 
-
-else
-    num_called=$(grep -E 'UNK|SOM' "${peak_file}" | \
-        awk -v FS='\t' '{print $4}' | sort | uniq | wc -l)
-
-    tp=$(grep -E 'UNK|SOM' "${peak_file}" | \
-        awk -v FS='\t' -v OFS='\t' '{if ($15 > 0) print $4,$7}' | \
-        awk -v FS='\t' '{print $1}' | sort | uniq | wc -l) 
-fi
-
-fp=$((${num_called} - ${tp}))
 fn=$((${num_true} - ${tp}))
 
 precision=$(echo "scale=4; ${tp} / (${tp} + ${fp})" | bc) 
 recall=$(echo "scale=4; ${tp} / (${tp} + ${fn})" | bc) 
 f1=$(echo "scale=4; 2 * (${precision} * ${recall}) / (${precision} + ${recall})" | bc)
 
-echo -e "Number of true insertions: ${num_true}\n" > "${out_file}"
-echo -e "BEFORE ERROR-PRONE FILTER: ${sample}" >> "${out_file}"
+echo -e "Number of true insertions: ${num_true}" > "${out_file}"
 echo "Number of UNK/SOM peaks called by HAT-seq: ${num_called}" >> "${out_file}"
 echo "Number of true peaks found by HAT-seq: ${tp}" >> "${out_file}"
 echo "Precision: ${precision}" >> "${out_file}"
 echo "Recall: ${recall}" >> "${out_file}"
 echo "F1-score: ${f1}" >> "${out_file}"
-
-
-#### AFTER ERROR-PRONE FILTER ####
-
-if [[ $# == 5 ]]; then
-    num_called_noerr=$(grep -v -f "${names}" "${noerr_file}" | grep -E 'UNK|SOM' | \
-        awk -v FS='\t' '{print $4}' | sort | uniq | wc -l)
-
-    tp_noerr=$(grep -v -f "${names}" "${noerr_file}" | grep -E 'UNK|SOM' | \
-        awk -v FS='\t' -v OFS='\t' '{if ($15 > 0) print $4,$7}' | \
-        awk -v FS='\t' '{print $1}' | sort | uniq | wc -l) 
-else
-    num_called_noerr=$(grep -E 'UNK|SOM' "${noerr_file}" | \
-        awk -v FS='\t' '{print $4}' | sort | uniq | wc -l)
-
-    tp_noerr=$(grep -E 'UNK|SOM' "${noerr_file}" | \
-        awk -v FS='\t' -v OFS='\t' '{if ($15 > 0) print $4,$7}' | \
-        awk -v FS='\t' '{print $1}' | sort | uniq | wc -l) 
-fi
-
-fp_noerr=$((${num_called_noerr} - ${tp_noerr}))
-fn_noerr=$((${num_true} - ${tp_noerr}))
-
-precision_noerr=$(echo "scale=4; ${tp_noerr} / (${tp_noerr} + ${fp_noerr})" | bc) 
-recall_noerr=$(echo "scale=4; ${tp_noerr} / (${tp_noerr} + ${fn_noerr})" | bc) 
-f1_noerr=$(echo "scale=4; 2 * (${precision_noerr} * ${recall_noerr}) / (${precision_noerr} + ${recall_noerr})" | bc)
-
-echo -e "\nAFTER ERROR-PRONE FILTER: ${sample}" >> "${out_file}"
-echo "Number of UNK/SOM peaks called by HAT-seq: ${num_called_noerr}" >> "${out_file}"
-echo "Number of true peaks found by HAT-seq: ${tp_noerr}" >> "${out_file}"
-echo "Precision: ${precision_noerr}" >> "${out_file}"
-echo "Recall: ${recall_noerr}" >> "${out_file}"
-echo "F1-score: ${f1_noerr}" >> "${out_file}"
--- workflow/run_post-process.sh
+++ workflow/run_post-process.sh
@@ -1,61 +1,35 @@
 #!/bin/bash
 
-# Post-process each sample by filtering out error-prone regions, comparing to truth set, 
-# and calculating statistics.
+# Post-process each sample by calculating statistics.
 
 resdir=$(realpath $1) # Parent directory with pipeline results
 tdir=$(realpath $2) # Directory with truth sets
-err=$3 # Error-prone regions to remove
-genome=$4 # Chrom sizes
 
 all_true="${tdir}/l1_hapmapmixture_final_v2_all_tier1-2.bed"
 som_true="${tdir}/l1_hapmapmixture_final_v2_somatic_tier1-2.bed"
 target_true="${tdir}/l1_hapmapmixture_final_v2_target_tier1-2.bed"
 som_target_true="${tdir}/l1_hapmapmixture_final_v2_target_somatic_tier1-2.bed"
 
-# Find lists of insertions to remove for each subset GTS
-# e.g. If comparing to somatic GTS, remove germline GTS insertions
-grep germline "${all_true}" | \
-    awk -v FS='\t' '{print $4}' \
-    > "${tdir}/l1_hapmapmixture_final_v2_germline_ins.txt"
-
-awk -v FS='\t' '{print $4}' "${target_true}" | \
-    grep -v -f - "${all_true}" | \
-    awk -v FS='\t' '{print $4}' \
-    > "${tdir}/l1_hapmapmixture_final_v2_non-target_ins.txt"
-
-cat "${tdir}/l1_hapmapmixture_final_v2_germline_ins.txt" "${tdir}/l1_hapmapmixture_final_v2_non-target_ins.txt" \
-    > "${tdir}/l1_hapmapmixture_final_v2_germline_non-target_ins.txt"
-
 for path in $(ls ${resdir}/); do
     sample=$(basename "${path}")
     echo $sample
     
     indir="${resdir}/${sample}/analysis"
-    outdir="${resdir}/${sample}/analysis/post"
+    outdir="${resdir}/${sample}/analysis/stats"
     mkdir -p "${outdir}"
     
-    big_table="${indir}/${sample}_big_table_filter_reasons.tsv"
-    out_file="${outdir}/${sample}_big_table_filter_all_tier1-2.tsv"
-    noerr_file="${outdir}/${sample}_big_table_filter_all_tier1-2_noerror.tsv"
-
-    # Compare HAT-seq peaks to each GTS and filter out peaks in error-prone regions
-    bash compare_truth_and_filter_error.sh \
-        "${big_table}" "${out_file}" "${noerr_file}" "${all_true}" "${err}" "${genome}"
+    big_table="${indir}/${sample}_filtered_peaks.tsv"
 
     # Calculate precision/recall for each GTS  
     bash calculate_stats.sh \
-        "${out_file}" "${noerr_file}" "${outdir}/${sample}_f1-score_all_tier1-2.txt" "${all_true}" 
+        "${big_table}" "${all_true}" "${outdir}/${sample}_f1-score_all_tier1-2.txt"
 
     bash calculate_stats.sh \
-        "${out_file}" "${noerr_file}" "${outdir}/${sample}_f1-score_somatic_tier1-2.txt" "${som_true}" \
-        "${tdir}/l1_hapmapmixture_final_v2_germline_ins.txt"
+        "${big_table}" "${som_true}" "${outdir}/${sample}_f1-score_somatic_tier1-2.txt"
 
     bash calculate_stats.sh \
-        "${out_file}" "${noerr_file}" "${outdir}/${sample}_f1-score_target_tier1-2.txt" "${target_true}" \
-        "${tdir}/l1_hapmapmixture_final_v2_non-target_ins.txt"
+        "${big_table}" "${target_true}" "${outdir}/${sample}_f1-score_target_tier1-2.txt"
 
     bash calculate_stats.sh \
-        "${out_file}" "${noerr_file}" "${outdir}/${sample}_f1-score_target_somatic_tier1-2.txt" "${som_target_true}" \
-        "${tdir}/l1_hapmapmixture_final_v2_germline_non-target_ins.txt"
+        "${big_table}" "${som_target_true}" "${outdir}/${sample}_f1-score_target_somatic_tier1-2.txt"
 done  
